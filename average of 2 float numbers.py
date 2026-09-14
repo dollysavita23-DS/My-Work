c76f706b321c7d1c70f8da265d681f795e5ff3a7
dollysavita23-DS <dollysavita.23@gmail.com>
2026-09-14 22:27:05 +0530

    Add files via upload

---
+++ average of 2 float numbers.py
@@ -0,0 +1,8 @@
+# To input two floating numbers and find their average
+
+a = float(input("Enter the Number : "))
+b = float(input("Enter the Number : "))
+
+average = (a+b)/2
+
+print("Average of a and b is : ",average)
