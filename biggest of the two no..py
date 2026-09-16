2670f576fc5f96e6938bb4f19b47297ab292a241
dollysavita23-DS <dollysavita.23@gmail.com>
2026-09-16 09:44:43 +0530

    Add files via upload

---
+++ biggest of the two no..py
@@ -0,0 +1,9 @@
+# To find the Biggest of the two numbers
+
+num1 = int(input("Enter the number : "))
+num2 = int(input("Enter the number : "))
+
+if(num1 > num2):
+    print("Num1 is Biggest")
+else:
+    print("Num2 is Biggest")
