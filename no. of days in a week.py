1721bc9205bc541860c87c1a567a31937fd4d162
dollysavita23-DS <dollysavita.23@gmail.com>
2026-09-16 09:48:42 +0530

    Add files via upload

---
+++ no. of days in a week.py
@@ -0,0 +1,18 @@
+# To find no. of days in a week
+
+Week_Day = input("Enter week day's name : ")
+
+if(Week_Day == "Monday"):
+    print("Week Day Number is 1")
+elif(Week_Day == "Teusday"):
+    print("Week Day Number is 2")
+elif(Week_Day == "Wednesday"):
+    print("Week Day Number is 3")
+elif(Week_Day == "Thursday"):
+    print("Week Day Number is 4")
+elif(Week_Day == "Friday"):
+    print("Week Day Number is 5")
+elif(Week_Day == "Saturday"):
+    print("Week Day Number is 6")
+elif(Week_Day == "Sunday"):
+    print("Week Day Number is 7")
