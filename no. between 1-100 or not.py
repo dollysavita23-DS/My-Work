8b15b305848216514c2329be3358daffa646375e
dollysavita23-DS <dollysavita.23@gmail.com>
2026-09-16 09:48:22 +0530

    Add files via upload

---
+++ no. between 1-100 or not.py
@@ -0,0 +1,8 @@
+# To check whether the no. is between 1-100 or not?
+
+num = int(input("Enter the number : "))
+
+if num>=1 and num<=100:
+    print("Number is between 1 to 100")
+else:
+    print("Number is not between 1 to 100")
