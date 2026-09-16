6b601c5b5d74a92d9c98d802551a6a4b67d05a55
dollysavita23-DS <dollysavita.23@gmail.com>
2026-09-16 09:49:54 +0530

    Add files via upload

---
+++ print name n no. of times.py
@@ -0,0 +1,7 @@
+# To print your name n No. of times
+
+name = input("Enter your name : ")
+n = int(input("Enter how many times you want to print your name : "))
+
+for i in range(n):
+    print(name)
