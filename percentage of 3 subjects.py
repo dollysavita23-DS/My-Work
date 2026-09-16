3581f0602c778b61882d9f4ae8b2e2964ea4830c
dollysavita23-DS <dollysavita.23@gmail.com>
2026-09-16 09:48:56 +0530

    Add files via upload

---
+++ percentage of 3 subjects.py
@@ -0,0 +1,9 @@
+# To find percentage of 3 subjects
+
+sub1 = float(input("Enter the marks : "))
+sub2 = float(input("Enter the marks : "))
+sub3 = float(input("Enter the marks : "))
+
+percentage = (sub1 + sub2 + sub3)/3
+
+print("Percentage of three subjects are : ",percentage)
